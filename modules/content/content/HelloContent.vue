47db73dc86c7cd7d7d3e522a5cbddf742878de27
caicodes <caicodes1@gmail.com>
2022-08-18 19:08:33 -0600

    improved latest conten module imported and setup

--- modules/content/content/HelloContent.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <main>
-        <ContentDoc class="prose" />
-    </main>
-</template>
--- modules/content/content/Viewer.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-const { data } = await useAsyncData('hello', () => queryContent('/global/hello').findOne())
-</script>
-<template>
-    <ContentRenderer :value="data" class="prose">
-        <slot />
-    </ContentRenderer>
-</template>
